f9a01582d17ce451f2a1f2b5d1c630d2f4dccc8c
“Manuel <manuelradaljac@gmail.com>
2024-02-13 21:57:23 +0100

    Add @vercel/speed-insights package

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@/app/globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function AuthLayout({
               {children}
             </ThemeProvider>
             <Analytics />
+            <SpeedInsights/>
           </body>
         </html>
     </>
